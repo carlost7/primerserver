d8c9bce467f56fda1910863bad11624916b8d690
carlos <carlos@carlos>
2014-11-05 17:53:52 -0600

    add libraries

--- app/events.php
+++ app/events.php
@@ -7,3 +7,21 @@
  */
 
 //Event::listen('pago_aprobado', 'PagosListener@publicar_contenido');
+Event::listen('eloquent.creating: Domain', 'DomainListener@store');
+Event::listen('eloquent.creating: Domain', 'DomainListener@update');
+Event::listen('eloquent.creating: Domain', 'DomainListener@destroy');
+
+Event::listen('eloquent.creating: Ftp', 'FtpListener@store');
+Event::listen('eloquent.creating: Ftp', 'FtpListener@update');
+Event::listen('eloquent.creating: Ftp', 'FtpListener@destroy');
+
+Event::listen('eloquent.creating: Email', 'EmailListener@store');
+Event::listen('eloquent.creating: Email', 'EmailListener@update');
+Event::listen('eloquent.creating: Email', 'EmailListener@destroy');
+
+Event::listen('eloquent.creating: Database', 'DatabaseListener@store');
+Event::listen('eloquent.creating: Database', 'DatabaseListener@update');
+Event::listen('eloquent.creating: Database', 'DatabaseListener@destroy');
+
+
+
--- app/start/global.php
+++ app/start/global.php
@@ -16,6 +16,8 @@ ClassLoader::addDirectories(array(
     app_path() . '/controllers',
     app_path() . '/models',
     app_path() . '/database/seeds',
+    app_path() . '/library',    
+    app_path() . '/listeners'
 ));
 
 /*
