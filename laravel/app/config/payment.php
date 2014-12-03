3a7caaaf7efb9894b9cb824d471efa8a27aa08ec
carlos <carlos@carlos>
2014-12-03 17:11:24 -0600

    checar pagos

--- laravel/app/config/payment.php
+++ laravel/app/config/payment.php
@@ -3,7 +3,7 @@
 return array(
     'client_id'     => $_ENV['MP_CLIENT_ID'],
     'client_socket' => $_ENV['MP_CLIENT_SOCKET'],
-    'init_point'    => 'init_point',
+    'init_point'    => 'sandbox_init_point',
     'sandbox_mode'  => true,
 );
 
