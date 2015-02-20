080370d803749c5186ab69d11819d4e4848baad6
JorgeBrito <JorgeBrito@192.168.15.19>
2015-02-20 11:55:27 -0600

    hice artos cambio de borrar login en barra de menu ...cambiar menu para paginas internas y algo asi

--- laravel/app/views/domains/show.blade.php
+++ laravel/app/views/domains/show.blade.php
@@ -3,16 +3,15 @@
 @section('contenido')
 <div class="container">
     <div class="row">
-        <ul class="nav nav-tabs" role="tablist">
-            <li>{{HTML::LinkRoute('user.domains.index',trans('frontend.link.domain.index'),$user->id)}}</li>
-        </ul>
-    </div>
-    <div class="row">
         <div class="col-xs-12">
             <h2>{{$domain->domain}}</h2>
         </div>
     </div>
-    <div class="row">            
+    
+    <div class="row"> 
+        
+        @include('layouts.menu', ['page' => 'principal'])
+        
         <div class="col-sm-4">
             <h2>{{ HTML::linkRoute('user.emails.index',trans('frontend.link.email.index'),array('user_id'=>$user->id,'domain_id'=>$domain->id)) }}</h2>
             <h2>{{ HTML::linkRoute('user.emails.create',trans('frontend.link.email.create'),array('user_id'=>$user->id,'domain_id'=>$domain->id)) }}</h2>
